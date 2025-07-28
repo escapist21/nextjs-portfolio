405af34e477b991a8fef8681df36101999b2500d
Sourav <ad.sourav21@live.com>
2025-07-28 15:08:03 +0530

    updated app

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,27 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 className="mb-8 text-2xl font-semibold tracking-wider">
+        Sourav's Portfolio
       </h1>
+      <h2 className="mb-2 text-xl font-semibold tracking-normal">
+        Data Scientist | Systems Thinker | Mixed-methods Researcher
+      </h2>
+      <h2 className="mb-8 text-xl font-normal tracking-normal">
+        Building AI & Data Tools for Climate, Migration & Policy Impact
+      </h2>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I’m a full-stack data science engineer working at the intersection of public policy, 
+        climate resilience, and social impact. My work spans the design and automation of complex 
+        data pipelines, geospatial and mixed-methods analysis, and building AI-enabled systems 
+        that inform real-world decisions.`}
+      </p>
+      <p className='mb-4'>
+        {`Currently, I focus on leveraging data-driven insights to unpack the systemic links between 
+        climate change, migration, and agriculture especially in contexts like India's just energy 
+        transition, labour mobility, and digital literacy. I’ve collaborated with UN agencies, 
+        academic institutions, and social enterprises to translate research into actionable tools, 
+        policy briefs, and digital platforms.`}
       </p>
       <div className="my-8">
         <BlogPosts />
