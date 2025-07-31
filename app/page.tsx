3029435981bc502fa2a578baea53e07dbe77f9f9
Sourav <ad.sourav21@live.com>
2025-07-31 09:39:04 +0530

    updated profile section

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,42 @@ import Image from 'next/image'
 
 export default function Page() {
   return (
-    <section className='flex space-x-16 h-screen'>
-      <div className='flex w-1/3 flex-col rounded-2xl h-2/3 border-6 border-emerald-500' id='Social Section'>
+    <section className='flex space-x-16'>
+      <div className='flex w-1/3 flex-col rounded-2xl border-6 border-emerald-500 py-10 px-4' id='Social Section'>
         <div className="flex justify-center">
           {/* <img className="my-10 rounded-md" src="https://placehold.co/240x280" /> */}
-          <Image 
-            className='my-10 rounded-lg'
+          <Image
+            className='mb-10 rounded-lg'
             src="/images/headshot_rect_yellow.png"
             alt="A profile image of Sourav Adhikari"
             width={240}
             height={300}
           />
         </div>
-        <h3 className="mb-4 text-center text-5xl font-extrabold font-stretch-condensed tracking-tight">Sourav Adhikari</h3>
-        <p className="text-center text-xl font-stretch-extra-condensed">Data Scientist | Systems Thinker | Researcher</p>
-        <p className="text-center text-xl ">New Delhi, India</p>
-        <ul className='flex flex-row mt-6 px-24 justify-evenly'>
-          <li><a className="hover:text-emerald-500" rel="noopener noreferrer" target="_blank" href='href="https://www.linkedin.com/in/sourav90/'><i className='fa-brands fa-linkedin-in'></i></a></li>
-          <li><a className="hover:text-emerald-500" rel="noopener noreferrer" target="_blank" href='https://github.com/escapist21/'><i className='fa-brands fa-github'></i></a></li>
-          <li><a className="hover:text-emerald-500" rel="noopener noreferrer" target="_blank" href="mailto:sourav90.adhikari@gmail.com"><i className='fa-solid fa-envelope'></i></a></li>
-        </ul>
+        <div className='flex flex-col'>
+          <h3 className="mb-4 text-center text-5xl font-extrabold font-stretch-condensed tracking-tight">Sourav Adhikari</h3>
+          <p className="text-center text-xl font-stretch-extra-condensed">Data Scientist | Systems Thinker | Researcher</p>
+          <p className="text-center text-xl ">New Delhi, India</p>
+          <ul className='flex flex-row mt-6 px-24 justify-evenly'>
+            <li>
+              <a className="hover:text-emerald-500" rel="noopener noreferrer" target="_blank" href='href="https://www.linkedin.com/in/sourav90/'>
+                <i className='fa-brands fa-linkedin-in'></i>
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-emerald-500" rel="noopener noreferrer" target="_blank" href='https://github.com/escapist21/'>
+                <i className='fa-brands fa-github'></i>
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-emerald-500" rel="noopener noreferrer" target="_blank" href="mailto:sourav90.adhikari@gmail.com">
+                <i className='fa-solid fa-envelope'></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className='flex flex-col w-2/3 text-left h-2/3'>
+      <div className='flex flex-col w-2/3 text-left'>
         <h1 className="text-6xl font-extrabold font-stretch-50% tracking-tight">
           Building AI & Data Tools for
         </h1>
