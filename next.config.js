9a99c71d06ae67daed3ec2126cda1a3cd1016d6e
Sourav <ad.sourav21@live.com>
2025-07-31 19:27:51 +0530

    fixed config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-// next.config.js
-module.exports = {
-  // ... rest of the configuration.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "standalone",
 };
+
+module.exports = nextConfig;
