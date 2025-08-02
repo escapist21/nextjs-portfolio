70bbf686bbfeb95b4b425f33cbaced3c184fdffd
Sourav <ad.sourav21@live.com>
2025-08-02 10:02:24 +0530

    added config to use cloudinary with next/images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dkf3vfz08/image/upload/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
